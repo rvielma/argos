691e40e170e562e6b65a36ceec36eccf8351ff36
Raphael Vielma <rvielma@gmail.com>
2026-03-07 23:18:34 -0300

    chore: bump version to 2.2.1

--- Formula/argos.rb
+++ Formula/argos.rb
@@ -1,7 +1,7 @@
 class Argos < Formula
   desc "Web Security Scanner for Healthcare Environments"
   homepage "https://github.com/rvielma/argos"
-  version "2.2.0"
+  version "2.2.1"
   license "MIT"
 
   on_macos do
